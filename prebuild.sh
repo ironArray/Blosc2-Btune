3aae5392fd788d3810693ddcad757ad3060a39c0
Francesc Alted <francesc@blosc.org>
2023-05-17 17:52:27 +0200

    Allow different wheels to build in one go

--- prebuild.sh
+++ prebuild.sh
@@ -2,11 +2,28 @@
 
 # This should be executed in the root directory of the project!
 
+# Checkout TensorFlow sources
 # v2.11.0 works both on Linux and Mac
-# git clone --depth=1 -b v2.11.0 https://github.com/tensorflow/tensorflow.git tensorflow_src
 # v2.12.0 does not seem to work on neither Linux nor Mac (and static compiling)
-# git clone --depth=1 -b v2.12.0 https://github.com/tensorflow/tensorflow.git tensorflow_src
-# v2.13.0-rc0 does seems to work again
-git clone --depth=1 -b v2.13.0-rc0 https://github.com/tensorflow/tensorflow.git tensorflow_src
-git clone --depth=1 https://github.com/Blosc/c-blosc2.git c-blosc2
-mkdir c-blosc2/build
+# v2.13.0-rc0 does seems to work again on both platforms
+TENSORFLOW_VERSION="v2.13.0-rc0"
+if [ ! -d "tensorflow_src" ]
+then
+  git clone --depth=1 -b $TENSORFLOW_VERSION https://github.com/tensorflow/tensorflow.git tensorflow_src
+else
+  echo "TensorFlow ($TENSORFLOW_VERSION) already cloned"
+fi
+
+# Checkout C-Blosc2 sources
+BLOSC2_VERSION="main"
+if [ ! -d "c-blosc2" ]
+then
+  git clone --depth=1 -b $BLOSC2_VERSION https://github.com/Blosc/c-blosc2.git c-blosc2
+fi
+if [ -d "c-blosc2/build" ]
+then
+  echo "C-Blosc2 ($BLOSC2_VERSION) already cloned"
+  rm -rf c-blosc2/build/*
+else
+  mkdir c-blosc2/build
+fi
