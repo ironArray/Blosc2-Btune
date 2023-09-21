e7b29cd020d60a9212a89307f23f32f9cabe5c82
Francesc Alted <francesc@blosc.org>
2023-09-21 13:55:44 +0200

    Post 1.0.2 release actions done

--- blosc2_btune/__init__.py
+++ blosc2_btune/__init__.py
@@ -7,7 +7,7 @@
 # See LICENSE.txt for details about copyright and rights to use.
 ##############################################################################
 
-VERSION = "1.0.2"
+VERSION = "1.0.3.dev"
 
 import os
 import platform
