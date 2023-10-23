a76365fb82b39c29b0cc6462de92cb986315ffcd
Francesc Alted <faltet@gmail.com>
2023-10-23 17:46:35 +0200

    Update btune_config.py

--- examples/btune_config.py
+++ examples/btune_config.py
@@ -14,11 +14,10 @@ import blosc2_btune
 import os
 
 
-urlpath = "btune_config.b2nd"
-
 # Create the SChunk
 rng = np.random.default_rng()
 a = rng.integers(low=0, high=10000, size=int(1e5), dtype=np.int64)
+
 # Set Btune parameters
 base_dir = os.path.dirname(__file__)
 kwargs = {
@@ -26,12 +25,10 @@ kwargs = {
     "perf_mode": blosc2_btune.PerformanceMode.DECOMP,
     "models_dir": f"{base_dir}/models/"}
 blosc2_btune.set_params_defaults(**kwargs)
+
 # Tell blosc2 to use btune by setting the corresponding tuner in the cparams
+urlpath = "btune_config.b2nd"
 _ = blosc2.asarray(a, urlpath=urlpath, mode="w", chunks=(5e3,),
                    cparams={"tuner": blosc2.Tuner.BTUNE})
 
-print("NDArray succesfully created!")
-
-_ = blosc2.asarray(a, mode="w", chunks=(5e3,), cparams={"tuner": blosc2.Tuner.BTUNE})
-
-# blosc2.remove_urlpath(urlpath)
+print(f"NDArray succesfully created in {urlpath}")
