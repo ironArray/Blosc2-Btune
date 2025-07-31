d755572e4faee16717d7289157ff66d2896cbdd2
Francesc Alted <francesc@blosc.org>
2025-07-31 18:48:51 +0200

    Attempt on tf v2.14.0

--- prebuild.sh
+++ prebuild.sh
@@ -7,7 +7,7 @@
 # v2.12.0 does not seem to work on neither Linux nor Mac (and static compiling)
 # v2.13.0-rc0 does seems to work again on both platforms
 # v2.15.0 does not seem to work on Win (error C1083: Cannot open include file: 'sys/mman.h': No such file or directory)
-TENSORFLOW_VERSION="v2.19.0"
+TENSORFLOW_VERSION="v2.14.0"
 if [ ! -d "tensorflow_src" ]
 then
   git clone --depth=1 -b $TENSORFLOW_VERSION https://github.com/tensorflow/tensorflow.git tensorflow_src
